aa0dcf12a096d92b063a793b5dd0d2b0c20d1bcc
Ian Guimarães <42502816+IanGuim@users.noreply.github.com>
2025-07-23 23:15:51 -0300

    Update test1.js

--- test1.js
+++ test1.js
@@ -11,3 +11,29 @@ voronoi(100, 5, 2)
   .modulateRepeat(osc(10), 3.0, 3.0, 0.5, 0.5))
   
   .out()
+
+
+
+gradient(0.1)
+  .color(1,5)
+  .repeat(40,40)
+  .rotate(360, 5)
+  .add(shape(3,0.2, 0.1).color(1).rotate(180, 50))
+  .rotate(180,40)
+  .add(noise(3,34))
+  .modulateScale(osc(2,2,2).kaleid(50).scale(0.5),15,0)  
+  .out()
+
+
+noise(20,2)
+  .add(voronoi(45,2))
+  .modulateScale(osc(2,2,2),15,0)
+  .color(2)
+  
+  .out()
+
+voronoi(23,9,10)
+  .color(0,1)
+  .add(noise(122, 5,200).color(0))
+  
+  .out()
